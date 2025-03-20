2e1f565ea6b46d985d0d8434d573ad88380c7898
gabrielfior <fior.gbm@gmail.com>
2025-03-20 13:53:45 -0400

    Add subgraph function

--- labs-mcp/src/labs_mcp/server.py
+++ labs-mcp/src/labs_mcp/server.py
@@ -1,17 +1,21 @@
-from mcp.server.fastmcp.server import FastMCP
+from typing import List
 
+from mcp.server.fastmcp.server import FastMCP
+from prediction_market_agent_tooling.config import APIKeys
+from prediction_market_agent_tooling.markets.agent_market import FilterBy
+from prediction_market_agent_tooling.markets.omen.data_models import OmenMarket
+from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
+    OmenSubgraphHandler,
+)
 
 mcp = FastMCP("Demo")
 
-# Add an addition tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
-
 
 @mcp.tool()
-def mul(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a * b
-
+def fetch_open_omen_markets(limit: int | None = None) -> list[OmenMarket]:
+    """Fetches Omen prediction markets on Gnosis that are still open for trading"""
+    s = OmenSubgraphHandler()
+    open_markets = s.get_omen_binary_markets_simple(
+        limit=limit, filter_by=FilterBy.OPEN
+    )
+    return open_markets
